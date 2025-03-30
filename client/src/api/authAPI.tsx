a3c3d6a4536807069c1371e6668bcbbf68977e04
granteckels <geckels1@gmail.com>
2025-03-29 19:01:21 -0500

    Client side auth that doesn't check server done

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,25 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch(
+      '/auth/login',
+      {
+        method: "POST",
+        body: JSON.stringify(userInfo)
+      }
+    );
 
-
+    if(!response.ok) {
+      throw new Error('invalid API response, check network tab!');
+    }
+    
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log('Error from data retrieval: ', err);
+    return [];
+  }
+};
 
 export { login };
--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,10 +3,12 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<JwtPayload>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    // Must check with server
   }
   
   isTokenExpired(token: string) {
@@ -15,16 +17,21 @@ class AuthService {
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem("idToken") ?? "";
   }
 
-  login(idToken: string) {
+  login(idToken: string): void {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem("idToken", idToken);
+    window.open("/");
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem("idToken");
+    window.open("/login");
   }
 }
 
