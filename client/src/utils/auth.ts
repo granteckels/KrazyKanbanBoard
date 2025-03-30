40a1e118ebd5356b541f5ecc94f79c72e0500f1a
granteckels <geckels1@gmail.com>
2025-03-30 18:06:16 -0500

    Finish utility functions

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -8,11 +8,18 @@ class AuthService {
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
-    // Must check with server
+    return this.getToken();
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    const decodedToken = jwtDecode<JwtPayload>(token);
+
+    const currentTime = Date.now() / 1000;
+    if(decodedToken.exp && decodedToken.exp < currentTime) {
+      return true;
+    }
+    return false;
   }
 
   getToken(): string {
@@ -23,15 +30,17 @@ class AuthService {
   login(idToken: string): void {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
-    localStorage.setItem("idToken", idToken);
-    window.open("/");
+    if (idToken) {
+      localStorage.setItem("idToken", idToken);
+      window.location.href = "/";
+    }
   }
 
-  logout() {
+  // logout(navigate: Function) {
+  logout(): void {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
     localStorage.removeItem("idToken");
-    window.open("/login");
   }
 }
 
