20a1f712ca68db1a48c114f20d2823404b96e54f
granteckels <geckels1@gmail.com>
2025-03-30 18:07:23 -0500

    Create validate token middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -5,6 +5,28 @@ interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction): void => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers['authorization']
+  if(!authHeader) {
+    res.sendStatus(403);
+    return;
+  }
+
+  const token = (authHeader! as string).split(' ')[1];
+
+  if(!token) {
+    res.sendStatus(403);
+    return;
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY!, (err: any, user: any): void => {
+    if(err) {
+      res.sendStatus(403);
+      return;
+    }
+
+    req.user = user as JwtPayload;
+    next()
+  });
 };
